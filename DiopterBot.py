b5d4f56514f034ac2b171c6fa246cec30aa6a3ed
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2023-06-17 03:15:23 +0100

    Big rewrite of code to support slash commands and fix bugs

--- DiopterBot.py
+++ DiopterBot.py
@@ -1,153 +1,113 @@
 import discord
-import logging
 import random
 import asyncio
+import logging
+from discord import app_commands
+from discord.ext import commands
+from key import TOKEN
+
+bot = commands.Bot(command_prefix = "!?!?!?!?!!!!!?!???@!@#!@#", intents = discord.Intents.all())
+bot.remove_command('help')
 
 logging.basicConfig(level=logging.INFO)
 
 easter_egg_played = False
+easter_egg_file_exists = True
 
-easter_eggs_file = open('easter_eggs.txt')
-easter_eggs = easter_eggs_file.readlines()
-easter_eggs_file.close()
+try:
+    easter_eggs_file = open('easter_eggs.txt')
+    easter_eggs = easter_eggs_file.readlines()
+    easter_eggs_file.close()
+except FileNotFoundError:
+    easter_egg_file_exists = False
 
-# thar be dragons here
-class DiopterBot(discord.Client):
-    async def on_ready(self):
-        print('Logged in as {0.user}'.format(self))
+@bot.event
+async def on_ready():
+    print("DiopterBot running")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
 
-    async def easter_egg(self, channel):
-        global easter_egg_played
-        if not easter_egg_played:
-            egg = random.randint(0, (len(easter_eggs) - 1))
-            await channel.send(easter_eggs[egg])
-        else:
-            return
-        easter_egg_played = True
-        await asyncio.sleep(1200)
-        easter_egg_played = False
+async def easter_egg(interaction):
+    global easter_egg_played
+    if not easter_egg_played and easter_egg_file_exists:
+        egg = random.randint(0, (len(easter_eggs) - 1))
+        await interaction.response.send_message(easter_eggs[egg])
+    else:
+        await interaction.response.send_message("¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁ ÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàá âãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀā ĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġ ĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁ łŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠš ŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽž", delete_after=0)
+    easter_egg_played = True
+    await asyncio.sleep(1200)
+    easter_egg_played = False
 
-    async def cm_to_diopters(self, channel, cm_value, differentials=0):
-        cm_value = abs(float(cm_value))
-        differentials = float(differentials)
-        if cm_value > 1000 or differentials > 40 or differentials < -40:
-            await self.easter_egg(channel)
-            return
-        try:
-            result = round((100/cm_value), 2)
-        except ZeroDivisionError:
-            await channel.send("You can't see anything? :grimacing:")
-            return
-        formatted_cms = format(cm_value, '.2f')
-        formatted_raw_result = format(result, '.2f')
-        result_with_diffs = result - differentials
-        if result_with_diffs % 0.25 >= 0.125:
-            formatted_result = format(0.25 * ((result_with_diffs // 0.25) + 1), '.2f')
-        else:
-            formatted_result = format(0.25 * (result_with_diffs // 0.25), '.2f')
-        result = format(result, '.2f')
-        differentials_formatted = format(differentials, '.2f')
-        result_with_diffs = format(result_with_diffs, '.2f')
-        if differentials == 0:
-            await channel.send("To calculate diopters, do 100 divided by the cm value, and add on a minus sign:\n`100/{}cm = -{} diopters\nMyopia of eye approximately -{}`\nFor help with DiopterBot, type `diopterbot`".format(formatted_cms, formatted_raw_result, formatted_result))
-        elif differentials < 0:
-            await channel.send("To calculate diopters, do 100 divided by the cm value, and add on a minus sign.\nBecause you measured with differentials, add the strength of your differentials as well:\n`100/{}cm = -{} diopters\n-{}{} = -{}\nMyopia of eye approximately -{}`\nFor help with DiopterBot, type `diopterbot`".format(formatted_cms, result, result, differentials_formatted, result_with_diffs, formatted_result))
-        elif differentials > 0:
-            await channel.send("To calculate diopters, do 100 divided by the cm value, and add on a minus sign.\nBecause you measured with plus lens, add the strength of your plus lens as well:\n`100/{}cm = -{} diopters\n-{}+{} = -{}\nMyopia of eye approximately -{}`\nFor help with DiopterBot, type `diopterbot`".format(formatted_cms, result, result, differentials_formatted, result_with_diffs, formatted_result))
+@bot.tree.command(name="cm-to-diopters", description="Converts a cm value to the diopter equivalent")
+@app_commands.describe(close_up_correction = "differentials")
+async def cm_to_diopters(interaction, cm: float, close_up_correction: float = 0.0):
+    cm = abs(cm)
+    if cm > 1000 or abs(close_up_correction) > 100:
+        await easter_egg(interaction)
+        return
+    try:
+        result = round((-100/cm), 2)
+    except ZeroDivisionError:
+        await interaction.response.send_message("You can't see anything? :face_with_monocle:")
+        return
+    result_with_diffs = result + (close_up_correction * -1)
+    if result_with_diffs % 0.25 >= 0.125:
+        formatted_result = format(0.25 * ((result_with_diffs // 0.25) + 1), '.2f')
+    else:
+        formatted_result = format(0.25 * (result_with_diffs // 0.25), '.2f')
 
-    async def diopters_to_cm(self, channel, diopter_value, differentials=0):
-        diopter_value = float(diopter_value)
-        differentials = float(differentials)
-        if diopter_value > 50 or diopter_value < -50 or differentials > 40 or differentials < -40:
-            await self.easter_egg(channel)
-            return
-        if diopter_value > differentials and differentials != 0:
-            await self.easter_egg(channel)
-            return
-        diopter_value = abs(float(diopter_value))
-        diopters_and_diffs = diopter_value + differentials
-        try:
-            result = round((100/float(diopter_value + differentials)), 2)
-        except ZeroDivisionError:
-            await channel.send(":infinity:\nIn theory, these are not your differentials but full strength glasses that correct you to perfect vision.\nYou can see ∞!")
-            return
-        diopter_value_formatted = format(diopter_value, '.2f')
-        # differentials_formatted = format(differentials, '.2f')
-        diopters_and_diffs_formatted = format(diopters_and_diffs, '.2f')
-        if differentials == 0:
-            await channel.send("To calculate diopters to cm, do 100 divided by the diopter value, ignoring the sign:\n`100/{} = {}cm`\nYou should be able to see {}cm with -{} myopia!\nFor help with DiopterBot, type `diopterbot`".format(diopter_value_formatted, result, result, diopter_value_formatted))
-        elif differentials < 0:
-            await channel.send("When wearing differentials, or any minus lens, the effective myopia of your eye reduces by the strength of the differentials, or glasses. We do the calculation with a diopter value of -{} instead of -{} because of this.\nTo calculate diopters from cm, do 100 divided by the diopter value, ignoring the sign:\n`100/{} = {}cm`\nYou should be able to see {}cm with -{} myopia!\nFor help with DiopterBot, type `diopterbot`".format(diopters_and_diffs_formatted, diopter_value_formatted, diopters_and_diffs_formatted, result, result, diopters_and_diffs_formatted))
-        elif differentials > 0:
-            await channel.send("If you are wearing plus lens the effective myopia of your eye increases by the strength of the lens. We do the calculation with a diopter value of -{} instead of -{} because of this.\nTo calculate diopters from cm, do 100 divided by the diopter value, ignoring the sign:\n`100/{} = {}cm`\nYou should be able to see {}cm with -{} myopia!\nFor help with DiopterBot, type `diopterbot`".format(diopters_and_diffs_formatted, diopter_value_formatted, diopters_and_diffs_formatted, result, result, diopters_and_diffs_formatted))
+    formatted_cms = format(cm, '.2f')
+    formatted_raw_result = format(result, '.2f')
+    result = format(result, '.2f')
+    differentials_formatted = format(close_up_correction, '.2f')
+    result_with_diffs = format(result_with_diffs, '.2f')
 
-    async def diopterbot_help(self, channel):
-        await channel.send(
-            "DiopterBot can calculate the diopter value from a cm value that you give it, and vice versa. It can also perform calculations with differentials and tells you how to work out calculations for the future.\n\nTo use DiopterBot, type `convert (value) (differentials)`, only giving a value for differentials if you want to include this in the calculation. Examples:\n`convert 7cm` will convert 7cm into diopters\n`convert -2` will tell you the number of cms you can see with -2 myopia\n`convert 20cm -1` will tell you your myopia value if you measured 20cm with a -1 lens (differential) in that eye.\nBot hosted by @Wolfo, coded by @NottNott")
+    myopia_or_hyperopia = 'Myopia'
+    if float(formatted_result) > 0:
+        formatted_result = '+' + formatted_result
+        result_with_diffs = '+' + result_with_diffs
+        myopia_or_hyperopia = 'Hyperopia'
 
-    async def on_message(self, message):
-        if message.content == self.user:
-            return
-        message.content = message.content.lower()
+    if close_up_correction == 0:
+        await interaction.response.send_message(f"To calculate diopters, do 100 divided by the cm value, and add on a minus sign:\n`100 / {formatted_cms}cm = {formatted_raw_result} diopters\nMyopia of eye approximately {formatted_result} diopters`")
+    elif close_up_correction < 0:
+        await interaction.response.send_message(f"To calculate diopters, do 100 divided by the cm value, and add on a minus sign:\n`100 / {formatted_cms}cm = {result} diopters`\nBecause you measured with close-up glasses, add the strength of your close-up glasses as well:`\n{result} - ({differentials_formatted}) = {result_with_diffs} diopters\n{myopia_or_hyperopia} of eye approximately {formatted_result} diopters`")
+    elif close_up_correction > 0:
+        await interaction.response.send_message(f"To calculate diopters, do 100 divided by the cm value, and add on a minus sign:\n`100 / {formatted_cms}cm = {result} diopters`\nBecause you measured with plus lenses, subtract the strength of your plus lenses as well:`\n{result} - (+{differentials_formatted}) = {result_with_diffs} diopters\n{myopia_or_hyperopia} of eye approximately {formatted_result} diopters`")
 
-        if message.content.lower().strip().startswith('convert'):
-            raw = message.content.lower()
-            request = raw.split()
-            # print(request)
-            # print(len(request))
-            if request[0] == 'convert' and len(request) < 2:
-                await self.diopterbot_help(message.channel)
-            if not 1 < len(request) < 4:
-                return
-            if len(request) == 2:
-                if request[1] == 'help':
-                    await self.diopterbot_help(message.channel)
-                elif request[1].endswith('c') or request[1].endswith('cm') or request[1].endswith('cms'):
-                    cm_value = request[1].split('c')[0]
-                    await self.cm_to_diopters(message.channel, cm_value)
-                elif request[1].startswith('-') or request[1].replace('.', '', 1).isdigit():
-                    if request[1].endswith('d') or request[1].endswith('diopters') or request[1].endswith('dioptres'):
-                        diopter_value = request[1].split('d')[0]
-                    else:
-                        diopter_value = request[1]
-                    await self.diopters_to_cm(message.channel, diopter_value)
-                elif request[1].endswith('d') or request[1].endswith('diopters') or request[1].endswith('dioptres'):
-                    diopter_value = request[1].split('d')[0]
-                    await self.diopters_to_cm(message.channel, diopter_value)
-            elif len(request) == 3:
-                if request[2].startswith('centi') or request[2].startswith('cm') or request[2].startswith('cms'):
-                    try:
-                        float(request[1])
-                    except ValueError:
-                        return
-                    cm_value = request[1]
-                    await self.cm_to_diopters(message.channel, cm_value)
-                elif request[2].startswith('d'):
-                    try:
-                        float(request[1])
-                    except ValueError:
-                        return
-                    diopter_value = request[1]
-                    await self.diopters_to_cm(message.channel, diopter_value)
-                elif request[1].endswith('cm') or request[1].endswith('cms') and request[2].startswith('-') or request[2].isdigit():
-                    if request[2].endswith('d') or request[2].endswith('diopters') or request[2].endswith('dioptres'):
-                        request[2] = request[2].split('d')[0]
-                    if request[1].endswith('d') or request[1].endswith('diopters') or request[1].endswith('dioptres'):
-                        request[1] = request[1].split('d')[0]
-                    cm_value = request[1].split('c')[0]
-                    await self.cm_to_diopters(message.channel, cm_value, differentials=request[2])
-                elif request[1].startswith('-') or request[1].isdigit() and request[2].startswith('-') or request[2].isdigit() or request[1].endswith('d') or request[1].endswith('diopters') or request[1].endswith('dioptres'):
-                    if request[2].endswith('d') or request[2].endswith('diopters') or request[2].endswith('dioptres'):
-                        request[2] = request[2].split('d')[0]
-                    if request[1].endswith('d') or request[1].endswith('diopters') or request[1].endswith('dioptres'):
-                        request[1] = request[1].split('d')[0]
-                    diopter_value = request[1]
-                    await self.diopters_to_cm(message.channel, diopter_value, differentials=request[2])
-                    
-        if message.content.lower().strip().startswith('diopterbot') and len(message.content.split()) < 3:
-            await self.diopterbot_help(message.channel)
+@bot.tree.command(name="diopters-to-cm", description="Converts a diopter value to the cm equivalent")
+@app_commands.describe(close_up_correction = "differentials")
+async def diopters_to_cm(interaction, diopters: float, close_up_correction: float = 0.0):
+    diopters = -abs(diopters)
+    if abs(diopters) > 100 or abs(close_up_correction) > 100:
+        await easter_egg(interaction)
+        return
+    if diopters > close_up_correction and close_up_correction != 0:
+        await easter_egg(interaction)
+        return
+    diopters_and_diffs = diopters - close_up_correction
+    try:
+        result = abs(round((100/diopters_and_diffs), 2))
+    except ZeroDivisionError:
+        await interaction.response.send_message(":infinity:\nIn theory, these are not your close-up glasses but full strength glasses that correct you to perfect vision.\nYou can see to ∞!")
+        return
+    diopter_value_formatted = format(abs(diopters), '.2f')
+    diopters_and_diffs_formatted = format(diopters_and_diffs, '.2f')
+    differentials_formatted = format(close_up_correction, '.2f')
+    diopters_and_diffs_formatted_no_sign = format(abs(diopters_and_diffs), '.2f')
+    if close_up_correction == 0:
+        await interaction.response.send_message(f"To calculate diopters to cm, do 100 divided by the diopter value, ignoring the sign:\n`100 / {diopter_value_formatted} = {result}cm`\nYou should be able to see {result}cm with {diopters_and_diffs_formatted} myopia!")
+    elif close_up_correction < 0:
+        await interaction.response.send_message(f"When wearing close-up glasses, or any minus lens, the effective myopia of your eye reduces by the strength of the glasses, and we do the next calculation with that value:\n`-{diopter_value_formatted} - ({differentials_formatted}) = {diopters_and_diffs_formatted} effective myopia`\nTo calculate diopters from cm, do 100 divided by the diopter value, ignoring all signs:\n`100 / {diopters_and_diffs_formatted_no_sign} = {result}cm`\nYou should be able to see {result}cm with {diopters_and_diffs_formatted} myopia!")
+    elif close_up_correction > 0:
+        await interaction.response.send_message(f"If you are wearing plus lenses the effective myopia of your eye increases by the strength of the lens, and we do the next calculation with that value:\n`-{diopter_value_formatted} - (+{differentials_formatted}) = {diopters_and_diffs_formatted} effective myopia`\nTo calculate diopters from cm, do 100 divided by the diopter value, ignoring all signs:\n`100 / {diopters_and_diffs_formatted_no_sign} = {result}cm`\nYou should be able to see {result}cm with {diopters_and_diffs_formatted} myopia!")
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = DiopterBot(intents=intents)
-client.run('TOKEN')
+@bot.tree.command(name="help", description="Provides help about DiopterBot")
+async def diopterbot_help(interaction):
+    await interaction.response.send_message(
+        "DiopterBot can calculate the diopter value from a cm value that you give it, and vice versa. It can also perform calculations with close-up glasses (differentials) and tells you how to work out calculations in the future.\n\nTo use DiopterBot, use the relevant slash command (/) with the required value and optional close-up value.")
+
+bot.run(TOKEN)
--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-![diopterbot](https://github.com/n0ttn0tt/DiopterBot/blob/main/diopterbot.png)
+![diopterbot](https://github.com/nottnottloop/DiopterBot/blob/main/diopterbot.png)
 # DiopterBot
 A Discord bot that calculates glasses diopter strength based on centimeter measurements and vice versa
 
-The code is complete spaghetti, but it works. If the bot had more planned functions I would parse input better and make it scaleable.
+~~The code is complete spaghetti, but it works. If the bot had more planned functions I would parse input better and make it scaleable.~~
+
+Now with slash commands, input is parsed loads better and the spaghetti is down significantly.
+
+Released under Creative Commons Attribution ShareAlike 4.0
+
+## Hosting the bot
+Create a file called 'key.py' in the same directory as diopterbot2 and create a variable called TOKEN with your Discord auth token
+
+`> key.py`
+`TOKEN = 'token goes here'`
+
+Optionally create a file called 'easter_eggs.txt', with an easter egg on each line to be displayed when someone requests something silly
